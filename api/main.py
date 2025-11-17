89e18e9cac75c7c927cb718db64d43139debd93e
sriram <sriram07.ms@gmail.com>
2025-11-17 17:15:52 +0530

    chore: allow additional local ports for CORS

--- api/main.py
+++ api/main.py
@@ -37,6 +37,10 @@ _cors_env = os.getenv("BACKEND_CORS_ORIGINS", "")
 _default_origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "http://localhost:3002",
+    "http://127.0.0.1:3002",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
